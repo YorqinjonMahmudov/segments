ef32841921e8d30bf9da6c8c687f6c69f4de963d
Yorqinjon Mahmudov <yorqinjonmahmudov93@gmail.com>
2022-08-07 17:15:43 +0000

    Update Segment.java

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -42,8 +42,9 @@ class Segment {
         double low = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
         double t = high / low;
 
-        // if (t >= 0 || t <= 1)
-        //     return null;
+          if ((x1>=x3 && y1>=y3 && x2>=x4))
+            return null;
+
 
         // return new Point(x1 + t * (x2 - x1), y1 + t * (y2 - y1));
 
